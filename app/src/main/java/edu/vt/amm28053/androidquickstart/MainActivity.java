8aa1d62e53bc798099cd21fd3d960808a0c55e28
Alex Marshall <amhokies@gmail.com>
2015-10-05 18:21:58 -0400

    App finished

--- app/src/main/java/edu/vt/amm28053/androidquickstart/MainActivity.java
+++ app/src/main/java/edu/vt/amm28053/androidquickstart/MainActivity.java
@@ -1,37 +1,101 @@
 package edu.vt.amm28053.androidquickstart;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /*
+     * These are our control buttons.
+     */
+    private Button start, stop, resume, reset;
+
+    private ChronometerMilli timer;
+
+    private long elapsedTime = 0L;
+
+    /**
+     * This is the first callback method called in the Activity lifecycle.
+     * Generally, this is where initialization of variables happens.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the layout of this activity to be the layout defined in activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // findViewById finds a View contained within the Activity's layout with the id passed in
+        // View ids are defined in the layout file.
+        start = (Button)findViewById(R.id.start);
+        stop = (Button)findViewById(R.id.stop);
+        resume = (Button)findViewById(R.id.resume);
+        reset = (Button)findViewById(R.id.reset);
+
+        timer = (ChronometerMilli)findViewById(R.id.chronometer);
+        resetTimer();
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        resume.setOnClickListener(this);
+        reset.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start:
+                startTimer();
+                hideButtons(start);
+                showButtons(stop);
+                break;
+            case R.id.stop:
+                pauseTimer();
+                hideButtons(stop);
+                showButtons(resume, reset);
+                break;
+            case R.id.resume:
+                startTimer();
+                hideButtons(resume, reset);
+                showButtons(stop);
+                break;
+            case R.id.reset:
+                resetTimer();
+                hideButtons(resume, reset);
+                showButtons(start);
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void showButtons(Button... buttons) {
+        for (Button b : buttons) {
+            b.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hideButtons(Button... buttons) {
+        for (Button b : buttons) {
+            b.setVisibility(View.GONE);
+        }
+    }
+
+    private void startTimer() {
+        timer.setBase(SystemClock.elapsedRealtime() - elapsedTime);
+        timer.start();
+    }
+
+    private void pauseTimer() {
+        elapsedTime = (SystemClock.elapsedRealtime() - timer.getBase());
+        timer.stop();
+    }
+
+    private void resetTimer() {
+        elapsedTime = 0;
+        timer.setText("00:00.00");
     }
 }
